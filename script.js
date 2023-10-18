a42bf8348a71bc8f9ffa1da6e3379385d80b2b07
luismagnum <luismagnum1@gmail.com>
2023-10-17 22:32:42 -0300

    scroll

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ window.onscroll = () =>{
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 }
+
+//scrollreveal
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '85px',
+  duration: 2500,
+  reset: false
+});
+
+sr.reveal('.home-text',{delay:300});
+sr.reveal('.home-img',{delay:400});
+sr.reveal('.container',{delay:400});
+sr.reveal('.about-img',{});
+sr.reveal('.about-text',{delay:300});
+sr.reveal('.middle-text',{});
+sr.reveal('.row-btn,.shop-content',{delay:300});
+sr.reveal('.review,.contact,.deve',{delay:300});
